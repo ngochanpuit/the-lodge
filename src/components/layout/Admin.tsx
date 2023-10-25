1be3858bbe3f73dd767bb50988983da3c18ce3fc
ngochanpuit <ngochan.puit@gmail.com>
2023-10-25 16:26:38 +0700

    chore: update call button

--- src/components/layout/Admin.tsx
+++ src/components/layout/Admin.tsx
@@ -2,6 +2,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Footer from '../Footer';
 import Header from '../Header';
 import { faPhone } from '@fortawesome/free-solid-svg-icons';
+import Link from 'next/link';
 
 interface AdminProps {
   children: React.ReactNode;
@@ -14,14 +15,14 @@ export default function Admin({ children }: AdminProps) {
       <div className="content_body">
         <div className="container">{children}</div>
       </div>
-      <a href="tel:+84794034322">
+      <Link href="tel:0794034322">
         <button
           type="button"
           className="z-[130] flex bg-green-800 bottom-24 right-7 w-12 h-12  text-white animate-bounce rounded-full fixed cursor-pointer justify-center items-center transition-all"
         >
           <FontAwesomeIcon className="w-6 h-6" icon={faPhone} />
         </button>
-      </a>
+      </Link>
 
       <Footer />
     </div>
